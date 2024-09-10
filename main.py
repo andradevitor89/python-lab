7c1bac60744469b65a9736de9d772de7fd4b582c
Vítor Alves <alves.vitor@bcg.com>
2024-09-10 10:00:18 -0300

    handling exceptions

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 import sys
 import time
 import asyncio
+import exceptiongroup
 import trio
-from start import start_many_async, start_many_sync
+from start import start_many_async, start_many_sync, start_many_async_trio
 
 if __name__ == "__main__":
     args = sys.argv[1:]
     mode = args[0]
     count = int(args[1])
-    if mode == "sync":
-        time_start = time.time()
-        start_many_sync(count=count)
-        time_end = time.time()
-        print(f"Time taken: {time_end - time_start}")   
-        
-    elif mode == "async":
-        time_start = time.time()
-        asyncio.run(start_many_async(count=count))
-        time_end = time.time()
-        print(f"Time taken: {time_end - time_start}")   
+    try:
+        if mode == "sync":
+            time_start = time.time()
+            start_many_sync(count=count)
+            time_end = time.time()
+            print(f"Time taken: {time_end - time_start}")   
+            
+        elif mode == "async":
+            time_start = time.time()
+            asyncio.run(start_many_async(count=count))
+            time_end = time.time()
+            print(f"Time taken: {time_end - time_start}")   
 
-    elif mode == "trio":
-        time_start = time.time()
-        asyncio.run(start_many_async(count=count))
-        time_end = time.time()
-        print(f"Time taken: {time_end - time_start}")   
-
-    elif mode == "trio":
-        time_start = time.time()
-        trio.run(start_many_async(count=count))
-        time_end = time.time()
-        print(f"Time taken: {time_end - time_start}")
+        elif mode == "trio":
+            time_start = time.time()
+            trio.run(start_many_async_trio, count)
+            time_end = time.time()
+            print(f"Time taken: {time_end - time_start}")
+    except exceptiongroup.ExceptionGroup as e:
+        print(f"Exception caught: {e}.")
+        sys.exit(1)    
+    except Exception as e:
+        print(f"Exception caught: {e}.")
+        sys.exit(1)
--- start.py
+++ start.py
@@ -1,19 +1,20 @@
 import asyncio
 import trio
-from print import print_async, print_sync
+from print import print_async, print_async_trio, print_sync
 
 def start_many_sync(count=1):
     for _ in range(count):
         print_sync()
 
-async def start_many_async(count=1):
+async def start_many_async(count:int):
     tasks = [print_async() for _ in range(count)]
-    await asyncio.gather(*tasks)
+    results = await asyncio.gather(*tasks)
 
-async def start_many_trio(count=1):
+
+async def start_many_async_trio(count:int):
+
     async with trio.open_nursery() as nursery:
         for _ in range(count):
-            nursery.start_soon(print_async)
-
+            nursery.start_soon(print_async_trio)
 
 
